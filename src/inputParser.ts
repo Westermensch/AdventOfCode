65f97a13c2854f90a319ed41082eccd6f869c8eb
Westermensch <christoph.westermann.privat@gmail.com>
2024-12-01 16:37:25 +0100

    Get first star of the day 1
    Trying typescript for fun, and working test driven with jest. Using a development container, but will have to clean up the npm modules i installed manually.

---
+++ src/inputParser.ts
@@ -0,0 +1,34 @@
+import { isUtf8 } from 'buffer';
+import * as fs from 'fs'
+import { number } from 'yargs';
+
+export const parseInputFile = (filePath: string): [number[], number[]] => {
+    const buffer: string = fs.readFileSync(filePath, 'utf-8');
+    const lines: string[] = buffer.split('\n');
+
+    const firstNumbers: number[] = [];
+    const secondNumbers: number[] = [];
+
+    lines.forEach(line => {
+        const [first, second] = line.split('   ').map(Number);
+        if (!isNaN(first) && !isNaN(second)) { firstNumbers.push(first); secondNumbers.push(second); }
+    });
+    return [firstNumbers, secondNumbers];
+}
+
+export const orderNumberAscending = (array: number[]): number[] => {
+    return array.sort((a, b) => a - b);
+}
+
+export const determineDifference = (firstNumbers: number[], secondNumbers: number[]): number =>{
+    if (firstNumbers.length !== secondNumbers.length) { throw new Error('Arrays must be of the same length');}
+    const diffs =  firstNumbers.map((value, index) => value - secondNumbers[index]);
+    return diffs.reduce((sum, diff) => sum + Math.abs(diff), 0);
+}
+
+export const integration = (path: string): number => {
+    const numberArrays: [number[], number[]] = parseInputFile(path);
+    const sorted1: number[] = orderNumberAscending(numberArrays[0]);
+    const sorted2: number[] = orderNumberAscending(numberArrays[1]);
+    return determineDifference(sorted1, sorted2);
+}
---
+++ tests/input.test.ts
@@ -0,0 +1,36 @@
+import * as Input from "../src/inputParser"
+
+test('parseInputFile should return the content of the exapmle.txt as string', () => {
+    const [firstNumbers, secondNumbers] = Input.parseInputFile('./tests/example.txt');
+    expect(firstNumbers).toEqual([3, 4, 2, 1, 3, 3]);
+    expect(secondNumbers).toEqual([4, 3, 5, 3, 9, 3]);
+});
+
+test('orderNumberAscending should return the number array sorted ascending', () => {
+    const testArray: number[] = [4, 3, 5, 3, 9, 3];
+    const sorted: number[] = Input.orderNumberAscending(testArray);
+    expect(sorted).toEqual([3, 3, 3, 4, 5, 9]);
+});
+
+test('orderNumberAscending should return the number array sorted ascending', () => {
+    const testArray: number[] = [3, 4, 2, 1, 3, 3];
+    const sorted: number[] = Input.orderNumberAscending(testArray);
+    expect(sorted).toEqual([1, 2, 3, 3, 3, 4]);
+});
+
+test('determineDifference should return added value of the abs differences of the array values', () => {
+    const testinput1: number[] = [1, 2, 3, 3, 3, 4];
+    const testinput2: number[] =  [3, 3, 3, 4, 5, 9];
+    const result: number = Input.determineDifference(testinput1, testinput2);
+    expect(result).toEqual(11);
+});
+
+test('integration should return added value of parsed input file', () => {
+    const result: number = Input.integration('./tests/example.txt');
+    expect(result).toEqual(11);
+});
+
+test('Too lazy to write a main function', () => {
+    const result: number = Input.integration('./tests/real.txt');
+    expect(result).toEqual(1938424);
+});
