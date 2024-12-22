b6984e9116d218b6613ec1f612faa6360a47e630
Westermensch <christoph.westermann.privat@gmail.com>
2024-12-22 16:46:28 +0100

    Finish day 3

--- day3/src/day3.ts
+++ day3/src/day3.ts
@@ -31,3 +31,28 @@ export const calculateResult = (inputArray: number[][]): number => {
     }
     return result;
 }
+
+export const extractMulStepsAndCommands = (input: string): string[]|null =>{
+    const regexPattern = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g; 
+    const matches = [...input.matchAll(regexPattern)];
+    const resultArray: string[] = matches.map(match => match[0]);
+    return resultArray;
+}
+
+export const followDoCommands = (input: string[]):string[] =>{
+    let stopCommand = false;
+    let resultArray: string[] = [];
+    for(let i = 0; i < input.length; i++){
+        if(input[i] === `don't()`){
+            stopCommand = true;
+        }
+        if(input[i] === `do()`){
+            stopCommand = false;
+        }
+        if(!stopCommand && input[i] !== `do()`&& input[i] !== `don't()`){
+            resultArray.push(input[i]);
+        }
+    }
+
+    return resultArray;
+}
--- day3/tests/day3.test.ts
+++ day3/tests/day3.test.ts
@@ -1,5 +1,7 @@
 import * as Day3 from "../src/day3"
 
+// Star 1
+
 test("readTestFileIntoString__givenExampleFile__RerturnAsString", ()=>{
     expect(Day3.readTestFileIntoString('./day3/tests/example.txt')).toBe('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))');
 })
@@ -29,4 +31,37 @@ test("calculateResult__givenRealNumbers__Return161", ()=>{
     let numberArray: number[][] = [];
     if(commandArray) numberArray = Day3.extractNumbers(commandArray);
     expect(Day3.calculateResult(numberArray)).toBe(189600467);
+})
+
+// Star 2
+
+test("extractMulStepsAndCommands__givenExampleString__RerturnArrayContainingAllMultiplyingSteps", ()=>{
+    const input = Day3.readTestFileIntoString('./day3/tests/example2.txt');
+    expect(Day3.extractMulStepsAndCommands(input)).toEqual(['mul(2,4)', `don't()`, 'mul(5,5)', 'mul(11,8)', `do()`, 'mul(8,5)']);
+})
+
+test("followDoCommands__givenParsedArray__RerturnArrayContainingAllMultiplyingStepsThatAreAllowed", ()=>{
+    const input = Day3.readTestFileIntoString('./day3/tests/example2.txt');
+    const commandArray = Day3.extractMulStepsAndCommands(input);
+    if(commandArray) expect(Day3.followDoCommands(commandArray)).toEqual(['mul(2,4)', 'mul(8,5)']);
+})
+
+test("calculateResult__givenExample2Numbers__Return48", ()=>{
+    const input = Day3.readTestFileIntoString('./day3/tests/example2.txt');
+    const commandArray = Day3.extractMulStepsAndCommands(input);
+    let numberArray: number[][] = [];
+    let reducedArray: string[] = [];
+    if (commandArray) reducedArray = Day3.followDoCommands(commandArray);
+    if(reducedArray) numberArray = Day3.extractNumbers(reducedArray);
+    expect(Day3.calculateResult(numberArray)).toBe(48);
+})
+
+test("calculateResult__givenRealNumbers__ReturnRealResult", ()=>{
+    const input = Day3.readTestFileIntoString('./day3/tests/real.txt');
+    const commandArray = Day3.extractMulStepsAndCommands(input);
+    let numberArray: number[][] = [];
+    let reducedArray: string[] = [];
+    if (commandArray) reducedArray = Day3.followDoCommands(commandArray);
+    if(reducedArray) numberArray = Day3.extractNumbers(reducedArray);
+    expect(Day3.calculateResult(numberArray)).toBe(107069718);
 })
