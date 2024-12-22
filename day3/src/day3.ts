9cfa2e73a33e36632251c01e7aa46936485d055e
Westermensch <christoph.westermann.privat@gmail.com>
2024-12-22 14:07:38 +0100

    Use Regex to extract muls from the example string
    Simple regex matching pattern to extract the requested commands
    from the example string. I decided to do this in an array, so debugging is easier.

--- day3/src/day3.ts
+++ day3/src/day3.ts
@@ -3,3 +3,9 @@ import * as fs from 'fs'
 export const readTestFileIntoString = (filePath:string) =>{
     return fs.readFileSync(filePath, 'utf-8');
 }
+
+export const extractMulSteps = (input: string): string[]|null => {
+    const regexPattern = /mul\(\d{1,3},\d{1,3}\)/g; 
+    const matches = input.match(regexPattern);
+    return matches;
+}
--- day3/tests/day3.test.ts
+++ day3/tests/day3.test.ts
@@ -3,3 +3,8 @@ import * as Day3 from "../src/day3"
 test("readTestFileIntoString__givenExampleFile__RerturnAsString", ()=>{
     expect(Day3.readTestFileIntoString('./day3/tests/example.txt')).toBe('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))');
 })
+
+test("extractMulSteps__givenExampleString__RerturnArrayContainingAllMultiplyingSteps", ()=>{
+    const input = Day3.readTestFileIntoString('./day3/tests/example.txt');
+    expect(Day3.extractMulSteps(input)).toEqual(['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']);
+})
